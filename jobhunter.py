341aea5e4ce26682793b5b4054bd3b162a90bb3b
AbiyuTamirat2 <148640715+AbiyuTamirat2@users.noreply.github.com>
2024-02-18 22:07:21 -0800

    Update changes

--- jobhunter.py
+++ jobhunter.py
@@ -10,17 +10,20 @@ import html2text
 # You may need to edit the connect function based on your local settings.#I made a password for my database because it is important to do so. Also make sure MySQL server is running or it will not connect
 def connect_to_sql():
     conn = mysql.connector.connect(user='root', password='',
-                                   host='127.0.0.1', database='cne340_jobhunter_Abiyu')
+                                   host='127.0.0.1', database='cne340_jobhunter_abiy')
     return conn
 
 
+# Create the table structure
 # Create the table structure
 def create_tables(cursor):
     # Creates table
     # Must set Title to CHARSET utf8 unicode Source: http://mysql.rjweb.org/doc.php/charcoll.
     # Python is in latin-1 and error (Incorrect string value: '\xE2\x80\xAFAbi...') will occur if Description is not in unicode format due to the json data
-    cursor.execute('''CREATE TABLE IF NOT EXISTS jobs (id INT PRIMARY KEY auto_increment, Job_id varchar(50) , 
-    company varchar (300), Created_at DATE, url varchar(30000), Title LONGBLOB, Description LONGBLOB ); ''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS jobs (id INT PRIMARY KEY auto_increment, job_id varchar(50) , 
+    company varchar (300), created_at DATE, url varchar(30000), title LONGBLOB, job_description LONGBLOB ); ''')
+    return
+
 
 
 # Query the database.
@@ -33,33 +36,34 @@ def query_sql(cursor, query):
 # Add a new job
 def add_new_job(cursor, jobdetails):
     # extract all required columns
-    job_id = jobdetails ["id"]
-    company = jobdetails["company"]
-    created_at = time.strptime(jobdetails["created_at"], "%a %b %d %H:%M:%S %Z %Y")
-    url = jobdetails["url"]
-    title = jobdetails["title"]
-    description = html2text.html2text(jobdetails['description'])
-    date = jobdetails['publication_date'][0:10]
-    query = cursor.execute("INSERT INTO jobs( Job_id, Company, Created_at, Url, Title, Description) "
-               "VALUES(%s,%s, %s, %s, %s, %s)", (job_id, company, created_at, url, title, description, date))
-     # %s is what is needed for Mysqlconnector as SQLite3 uses ? the Mysqlconnector uses %s
-    return query_sql(cursor, query)
+    job_id = jobdetails['id']
+    company = jobdetails['company']
+    created_at = time.strptime(jobdetails['created_at'], "%a %b %d %H:%M:%S %Z %Y")
+    url = jobdetails['url']
+    title = jobdetails['title']
+    job_description = html2text.html2text(jobdetails['description'])
+
+    cursor.execute("INSERT INTO jobs(job_id, company, created_at, url, title, description) "
+                   "VALUES(%s,%s, %s, %s, %s, %s)",
+                   (job_id, company, created_at, url, title, job_description))
+    # %s is what is needed for Mysqlconnector as SQLite3 uses ? the Mysqlconnector uses %s
+    return cursor
 
 
 # Check if new job
 def check_if_job_exists(cursor, jobdetails):
     ##Add your code here
-    title = jobdetails ["title"]
-    company = jobdetails ["company"]
-    query = "SELECT * FROM jobs WHERE Title = %s AND Company = %s"
-    return query_sql(cursor, query)
+    job_id = jobdetails['id']
+    cursor.execute("SELECT * FROM jobs WHERE job_id = %s", (job_id,))
+    return cursor.fetchall() is not None
+
 
 # Deletes job
-def delete_job(cursor, jobdetails):
+def delete_job(cursor, job_id):
     ##Add your code here
-    job_id = jobdetails["id"]
-    query = "DELETE FROM jobs WHERE Job_id = %s"
-    return query_sql(cursor, query)
+    query = "DELETE FROM jobs WHERE job_id = %s"
+    cursor.execute(query, (job_id,))
+    print(f"Job with ID {job_id} deleted from DB")
 
 
 # Grab new jobs from a website, Parses JSON code and inserts the data into a list of dictionaries do not need to edit
@@ -75,22 +79,19 @@ def jobhunt(cursor):
     # Fetch jobs from website
     jobpage = fetch_new_jobs()  # Gets API website and holds the json data in it as a list
     # use below print statement to view list in json format
-    # print(jobpage)
-    add_or_delete_job(jobpage, cursor)
+    print(jobpage)
+    add_or_delete_job(jobpage['jobs'], cursor)
 
 
 def add_or_delete_job(jobpage, cursor):
     # Add your code here to parse the job page
-    for jobdetails in jobpage['jobs']:  # EXTRACTS EACH JOB FROM THE JOB LIST. It errored out until I specified jobs. This is because it needs to look at the jobs dictionary from the API. https://careerkarma.com/blog/python-typeerror-int-object-is-not-iterable/
+    for jobdetails in jobpage:  # EXTRACTS EACH JOB FROM THE JOB LIST. It errored out until I specified jobs. This is because it needs to look at the jobs dictionary from the API. https://careerkarma.com/blog/python-typeerror-int-object-is-not-iterable/
         # Add in your code here to check if the job already exists in the DB
-        check_if_job_exists(cursor, jobdetails)
-        is_job_found = len(
-        cursor.fetchall()) > 0  # https://stackoverflow.com/questions/2511679/python-number-of-rows-affected-by-cursor-executeselect
-        if is_job_found:
+        if not check_if_job_exists(cursor, jobdetails):
             add_new_job(cursor, jobdetails)
-            print("New job added:", jobdetails['description'])
+            print(f"New job added to DB: {jobdetails['id']}")
         else:
-            print("Job already exists:", jobdetails['description'])
+            print(f"Job already exists in DB, {jobdetails['id']}")
 
 
 # Setup portion of the program. Take arguments and set up the script
@@ -102,6 +103,8 @@ def main():
     cursor = conn.cursor()
     create_tables(cursor)
 
+
+
     while (1):  # Infinite Loops. Only way to kill it is to crash or manually crash it. We did this as a background process/passive scraper
         jobhunt(cursor)
         time.sleep(21600)  # Sleep for 1h, this is ran every hour because API or web interfaces have request limits. Your reqest will get blocked.
@@ -111,4 +114,3 @@ def main():
 # If you want to test if script works change time.sleep() to 10 seconds and delete your table in MySQL
 if __name__ == '__main__':
     main()
-
